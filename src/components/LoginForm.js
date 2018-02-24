6153cf2e80207b8d2f21e5c1114fad5ffcf8452a
rahul <rahulhaque07@gmail.com>
2018-02-24 23:30:13 +0600

    Spinner component added

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {} from 'react-native';
+import {Text, View} from 'react-native';
 import {Card, CardSection, Button, Input} from './common';
 
 class LoginForm extends Component {
@@ -7,36 +7,73 @@ class LoginForm extends Component {
     state = {
         email: '',
         password: '',
+        error: ''
     };
+
+    onButtonPress() {
+        const {email, password} = this.state;
+
+        this.setState({error: ''});
+
+        firebase.auth.signInWithEmailAndPassword(email, password)
+            .catch(() => {
+                firebase.auth.createUserWithEmailAndPassword(email, password)
+                    .catch(() => {
+                        this.setState({error: 'Authentication failed!'});
+                    });
+            });
+    }
 
     render() {
         return (
-            <Card>
-                <CardSection>
-                    <Input
-                        autoCorrect={false}
-                        placeholder={'example@email.com'}
-                        label={'Email'}
-                        value={this.state.email}
-                        onChangeText={email => this.setState({ email })}
-                    />
-                </CardSection>
-                <CardSection>
-                    <Input
-                        secureTextEntry={true}
-                        autoCorrect={false}
-                        placeholder={'Enter your password'}
-                        label={'Password'}
-                        value={this.state.password}
-                        onChangeText={password => this.setState({ password })}
-                    />
-                </CardSection>
-                <CardSection>
-                    <Button title={'Login'}/>
-                </CardSection>
-            </Card>
+            <View>
+                <Card>
+                    <CardSection>
+                        <Input
+                            autoCorrect={false}
+                            placeholder={'example@email.com'}
+                            label={'Email'}
+                            value={this.state.email}
+                            onChangeText={email => this.setState({email})}
+                        />
+                    </CardSection>
+                    <CardSection>
+                        <Input
+                            secureTextEntry={true}
+                            autoCorrect={false}
+                            placeholder={'Enter your password'}
+                            label={'Password'}
+                            value={this.state.password}
+                            onChangeText={password => this.setState({password})}
+                        />
+                    </CardSection>
+                    <CardSection>
+                        <Text style={styles.errorTextStyle}>
+                            {this.state.error}
+                        </Text>
+                    </CardSection>
+                    <CardSection>
+                        <Button
+                            onPress={this.onButtonPress.bind(this)}
+                            title={'Login'}
+                        />
+                    </CardSection>
+                </Card>
+            </View>
         );
     }
 }
+
+const styles = {
+    errorTextStyle: {
+        alignSelf: 'center',
+        color: 'red',
+        justifyContent: 'center',
+    },
+    containerStyle: {
+        flex: 1,
+        justifyContent: 'center',
+    }
+};
 
 export default LoginForm;
